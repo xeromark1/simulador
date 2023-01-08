ce60c9f9d369be2aa70993165d5bd81320861de5
xeromark1 <122186114+xeromark1@users.noreply.github.com>
2023-01-08 10:27:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use(require('./archives/routes/comboboxDisenio'));
+//app.use(require('./archives/routes/comboboxDisenio'));
 
 /*
 // Routes
