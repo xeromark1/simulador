8b2ac333ac479e4547c8768274f5aa2c92778630
xeromark1 <122186114+xeromark1@users.noreply.github.com>
2023-01-07 22:34:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+/*
 // Routes
 app.use(require('./routes/index'));
-/*
+
 app.use('/public', express.static( 'public'));      //esto agrega todo lo que es css y js
 app.use('/generador.html', express.static( 'generador.html'));      //esto agrega todo lo que es css y js
 */
@@ -20,6 +20,7 @@ app.use('/generador.html', express.static( 'generador.html'));      //esto agreg
 app.get('/' , (request, response) => {
 
     response.sendFile(path.resolve(__dirname , "./archives/index.html"))
+    response.sendFile(path.resolve(__dirname , "./routes/index.js"))
 
 
 });
