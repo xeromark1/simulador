f96ff69476e8741f385681af2de68b9698105c5a
xeromark1 <122186114+xeromark1@users.noreply.github.com>
2023-01-08 10:36:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-app.use('/archives', express.static( 'archives'));      //esto agrega todo lo que es css y js
-app.use(require('./archives/routes/comboboxDisenio'));
+//app.use(require('./archives/routes/comboboxDisenio'));
 
 /*
 // Routes
